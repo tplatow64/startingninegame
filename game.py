a66faae70d5722f38dc860e13fef3949864fb4ff
Ty Platow <70657397+tplatow64@users.noreply.github.com>
2025-05-11 16:11:33 -0400

    game prototype v1

---
+++ game.py
@@ -0,0 +1,57 @@
+import csv
+import random
+
+def check_guesses(true_lineup, guesses):
+    total = len(guesses)
+    correct = 0
+    true_players = {}
+    for item in true_lineup:
+        true_pos = item['Pos']
+        true_player = item['Player']
+        true_players[true_pos] = true_player
+    
+    for pos in guesses:
+        if(true_players[pos].lower() == guesses[pos].lower()):
+            correct = correct + 1
+    show_guesses(true_players)
+    print(f"You got {correct} out of {total} right.")
+
+def show_guesses(guesses, year=None, team=None):
+    if(year != None and team != None):
+        print(f"{year} {team} Lineup:")
+    else:
+        print("Correct Answers:")
+    for item in guesses:
+        print(f"{item}: {guesses[item]}")
+
+with open('SampleData.csv') as file:
+    reader = csv.DictReader(file)
+    year = str(2008)
+    team = 'Philadelphia Phillies'
+    true_lineup = []
+    for row in reader:
+        if(row['Year'] == year and row['Team'] == team):
+            true_lineup.append(row)
+    guessed_lineup = {}
+    for player in true_lineup:
+        guessed_lineup[player['Pos']] = "_____"
+
+    pos_response = ""
+    player_response = ""
+    while not pos_response.lower() == 'submit':
+        show_guesses(guessed_lineup, year, team)
+        pos_response = input("Enter position, or submit: ").upper()
+        if(pos_response in guessed_lineup):
+            player_response = input(f"Enter guess at {pos_response}: ")
+            guessed_lineup[pos_response] = player_response
+        elif(pos_response.lower() == "submit"):
+            break
+        else:
+            print("Invalid position.")
+
+    #guessed_lineup = {'2B': 'chase utley', '1B': 'Ryan Howard', 'LF': 'Pat burrell', 'CF': '_____', 'SS': '_____', 'RF': 'Jayson Werth', '3B': '_____', 'C': '_____'}
+
+    check_guesses(true_lineup, guessed_lineup)
+
+
+
